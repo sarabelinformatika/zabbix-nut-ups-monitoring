f64156094b8b234dc9a83989ec3a9411bb30e693
SARABEL Informatika Kft. <info@sarabelinformatika.hu>
2026-07-13 14:35:21 +0200

    Add script to check Network UPS Tools status
    Script checks and lists configured Network UPS Tools devices.

--- scripts/check_nut.sh
+++ scripts/check_nut.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+echo "========================================"
+echo "Checking Network UPS Tools"
+echo "========================================"
+
+echo
+echo "Configured UPS devices:"
+echo
+
+upsc -l
+
+echo
+
+for UPS in $(upsc -l)
+do
+    echo "----------------------------------------"
+    echo "$UPS"
+    echo "----------------------------------------"
+
+    upsc "$UPS"
+
+    echo
+done
